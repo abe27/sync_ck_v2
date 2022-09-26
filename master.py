a8466fd67a1df3638f4956ebbad6a844fc7f2f23
Parzival Yuenyang <krumii.it@gmail.com>
2022-09-26 15:54:53 +0700

    upload edi

---
+++ master.py
@@ -0,0 +1,59 @@
+import csv
+import os
+import time
+import requests
+import urllib
+from dotenv import load_dotenv
+load_dotenv()
+
+api_host = os.getenv("API_HOST")
+api_user = os.getenv("API_USERNAME")
+api_password = os.getenv("API_PASSWORD")
+
+# login
+passwd = urllib.parse.quote(api_password)
+payload = f'username={api_user}&password={passwd}'
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+response = requests.request("POST", f"{api_host}/login", headers=headers, data=payload)
+auth = response.json()
+token = auth["data"]["jwt_token"]
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+# print(headers)
+### read user
+file = open('data/master/user.csv')
+csvreader = csv.reader(file)
+for r in csvreader:
+    username = r[0]
+    password = r[1]
+    email = r[2]
+    first_name = r[3]
+    last_name = r[4]
+    payload=f'username={username}&email={urllib.parse.quote(email)}&password={urllib.parse.quote(password)}&firstname={first_name}&lastname={last_name}'
+    response = requests.request("POST", f"{api_host}/register", headers=headers, data=payload)
+    print(f"Create User {username} Status: {response.status_code}")
+    time.sleep(0.1)
+
+print(f"----------------------------------------------------------------")
+
+### read consignee
+file = open('data/master/consignee.csv')
+csvreader = csv.reader(file)
+for r in csvreader:
+    whs = r[0]
+    factory = r[1]
+    affcode = r[2]
+    customer = r[3]
+    address = r[4]
+    prefix = r[5]
+    payload=f'whs_id={whs}&factory_id={factory}&affcode_id={affcode}&customer_id={customer}&customer_address_id={address}&prefix={prefix}&is_active=true'
+    response = requests.request("POST", f"{api_host}/consignee", headers=headers, data=payload)
+    print(f"Create Consignee {customer} Status: {response.status_code}")
+    time.sleep(0.1)
+
+### logout
+response = requests.request("GET", f"{api_host}/auth/logout", headers=headers, data={})
+print(response.text)
+
---
+++ upload_edi.py
@@ -0,0 +1,72 @@
+import csv
+from datetime import datetime, timedelta
+import os
+import time
+import requests
+import urllib
+from dotenv import load_dotenv
+load_dotenv()
+
+api_host = os.getenv("API_HOST")
+api_user = os.getenv("API_USERNAME")
+api_password = os.getenv("API_PASSWORD")
+
+# login
+passwd = urllib.parse.quote(api_password)
+payload = f'username={api_user}&password={passwd}'
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+response = requests.request(
+    "POST", f"{api_host}/login", headers=headers, data=payload)
+auth = response.json()
+token = auth["data"]["jwt_token"]
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+# print(headers)
+# read user
+list_dir = os.listdir(os.path.join(os.path.dirname(__file__), 'data/edi'))
+list_dir.sort()
+
+rn = 1
+for x in list_dir:
+    edi_dir = os.listdir(os.path.join('data/edi', x))
+    edi_dir.sort()
+    for i in edi_dir:
+        on_dir = os.path.join('data/edi', x)
+        filename = os.path.join(on_dir, i)
+        batch_id = str(i)[:7]
+        batch_name = str(i)[8:]
+        batch_date = datetime.strptime(
+            (str(i)[(len(batch_name) - 10):]).replace(".TXT", ""),
+            "%Y%m%d%H%M%S").strftime("%Y-%m-%dT%H:%M:%S%Z") + ".000Z"
+        payload = {
+            'mailbox_id': 'Y32V802',
+            'batch_no': batch_id,
+            'creation_on': batch_date,
+            'flags': 'C RT',
+            'format_type': 'A',
+            'originator': 'Y32TPS1',
+            'is_download': 'false',
+            'is_active': 'true'
+        }
+
+        # print(payload)
+        f = open(filename, 'rb')
+        files = [('file_edi', (batch_name, f, 'application/octet-stream'))]
+        response = requests.request("POST",
+                                    f'{api_host}/edi/file',
+                                    headers={
+                                        'Authorization': f'Bearer {token}', },
+                                    data=payload,
+                                    files=files)
+        f.close()
+        print(f"{rn} ==> upload file edi: {batch_name} status: {response.status_code}")
+        rn += 1
+    
+    time.sleep(5)
+
+# logout
+response = requests.request(
+    "GET", f"{api_host}/auth/logout", headers=headers, data={})
+print(response.text)
