6d6142388d6402c13c48a4cc8ea1be526b06dbfd
Taweechai Yuenyang <krumii.it@gmailcom>
2022-09-28 08:42:28 +0700

    add generate invoice

---
+++ generate_inv.py
@@ -0,0 +1,33 @@
+import csv
+from datetime import datetime, timedelta
+import os
+import time
+import requests
+import urllib
+from dotenv import load_dotenv
+load_dotenv()
+
+api_host = os.getenv("API_HOST")
+api_user = os.getenv("API_USERNAME")
+api_password = os.getenv("API_PASSWORD")
+
+# login
+passwd = urllib.parse.quote(api_password)
+payload = f'username={api_user}&password={passwd}'
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+response = requests.request(
+    "POST", f"{api_host}/login", headers=headers, data=payload)
+auth = response.json()
+token = auth["data"]["jwt_token"]
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+# generate invoice
+response = requests.request(
+    "PATCH", f"{api_host}/order/ent/generate", headers=headers, data={})
+print(f"generate invoice status: {response.status_code}")
+# logout
+response = requests.request(
+    "GET", f"{api_host}/auth/logout", headers=headers, data={})
+print(response.text)
