8f35040551ab385a72513f19de19dda9f3e5469b
Parzival Yuenyang <krumii.it@gmail.com>
2022-10-05 14:57:43 +0700

    update location

--- master.py
+++ master.py
@@ -22,70 +22,70 @@ headers = {
     'Authorization': f'Bearer {token}',
     'Content-Type': 'application/x-www-form-urlencoded'
 }
-# print(headers)
-# read user
-file = open(os.path.join(os.path.dirname(__file__), 'data/master/user.csv'))
-csvreader = csv.reader(file)
-for r in csvreader:
-    username = r[0]
-    password = r[1]
-    email = r[2]
-    first_name = r[3]
-    last_name = r[4]
-    payload = f'username={username}&email={urllib.parse.quote(email)}&password={urllib.parse.quote(password)}&firstname={first_name}&lastname={last_name}'
-    response = requests.request(
-        "POST", f"{api_host}/register", headers=headers, data=payload)
-    print(f"Create User {username} Status: {response.status_code}")
-    time.sleep(0.1)
+# # print(headers)
+# # read user
+# file = open(os.path.join(os.path.dirname(__file__), 'data/master/user.csv'))
+# csvreader = csv.reader(file)
+# for r in csvreader:
+#     username = r[0]
+#     password = r[1]
+#     email = r[2]
+#     first_name = r[3]
+#     last_name = r[4]
+#     payload = f'username={username}&email={urllib.parse.quote(email)}&password={urllib.parse.quote(password)}&firstname={first_name}&lastname={last_name}'
+#     response = requests.request(
+#         "POST", f"{api_host}/register", headers=headers, data=payload)
+#     print(f"Create User {username} Status: {response.status_code}")
+#     time.sleep(0.1)
 
-print(f"----------------------------------------------------------------")
+# print(f"----------------------------------------------------------------")
 
-# read consignee
-file = open(os.path.join(os.path.dirname(__file__), 'data/master/consignee.csv'))
-csvreader = csv.reader(file)
-for r in csvreader:
-    whs = r[0]
-    factory = r[1]
-    affcode = r[2]
-    customer = r[3]
-    address = r[4]
-    prefix = r[5]
-    payload = f'whs_id={whs}&factory_id={factory}&affcode_id={affcode}&customer_id={customer}&customer_address_id={address}&prefix={prefix}&is_active=true'
-    response = requests.request(
-        "POST", f"{api_host}/consignee", headers=headers, data=payload)
-    print(f"Create Consignee {customer} Status: {response.status_code}")
+# # read consignee
+# file = open(os.path.join(os.path.dirname(__file__), 'data/master/consignee.csv'))
+# csvreader = csv.reader(file)
+# for r in csvreader:
+#     whs = r[0]
+#     factory = r[1]
+#     affcode = r[2]
+#     customer = r[3]
+#     address = r[4]
+#     prefix = r[5]
+#     payload = f'whs_id={whs}&factory_id={factory}&affcode_id={affcode}&customer_id={customer}&customer_address_id={address}&prefix={prefix}&is_active=true'
+#     response = requests.request(
+#         "POST", f"{api_host}/consignee", headers=headers, data=payload)
+#     print(f"Create Consignee {customer} Status: {response.status_code}")
 
-# read group
-file = open(os.path.join(os.path.dirname(__file__), 'data/master/group.csv'))
-csvreader = csv.reader(file)
-for r in csvreader:
-    username = r[0]
-    whs = r[1]
-    factory = r[2]
-    affcode = r[3]
-    customer = r[4]
-    order_group = r[5]
-    sub_order = r[6]
-    payload = f'user_id={username}&whs_id={whs}&factory_id={factory}&affcode_id={affcode}&custcode_id={customer}&order_group_type_id={order_group}&sub_order={sub_order}&description=-&is_active=true'
-    response = requests.request(
-        "POST", f"{api_host}/order/consignee", headers=headers, data=payload)
-    print(
-        f"create consignee {username} ==> {affcode} customer:{customer} group is: {response.status_code}")
+# # read group
+# file = open(os.path.join(os.path.dirname(__file__), 'data/master/group.csv'))
+# csvreader = csv.reader(file)
+# for r in csvreader:
+#     username = r[0]
+#     whs = r[1]
+#     factory = r[2]
+#     affcode = r[3]
+#     customer = r[4]
+#     order_group = r[5]
+#     sub_order = r[6]
+#     payload = f'user_id={username}&whs_id={whs}&factory_id={factory}&affcode_id={affcode}&custcode_id={customer}&order_group_type_id={order_group}&sub_order={sub_order}&description=-&is_active=true'
+#     response = requests.request(
+#         "POST", f"{api_host}/order/consignee", headers=headers, data=payload)
+#     print(
+#         f"create consignee {username} ==> {affcode} customer:{customer} group is: {response.status_code}")
 
-# read loading area
-file = open(os.path.join(os.path.dirname(__file__), 'data/master/loading_area.csv'))
-csvreader = csv.reader(file)
-for r in csvreader:
-    bioat = r[0]
-    factory = r[1]
-    prefix = r[2]
-    loading_area = r[3]
-    privilege = r[4]
-    payload = f'bioat={bioat}&factory={factory}&prefix={prefix}&loading_area={loading_area}&privilege={privilege}&is_active=true'
-    response = requests.request(
-        "POST", f"{api_host}/order/loading", headers=headers, data=payload)
-    print(f"create loading Area {loading_area} is: {response.status_code}")
-    # time.sleep(0.1)
+# # read loading area
+# file = open(os.path.join(os.path.dirname(__file__), 'data/master/loading_area.csv'))
+# csvreader = csv.reader(file)
+# for r in csvreader:
+#     bioat = r[0]
+#     factory = r[1]
+#     prefix = r[2]
+#     loading_area = r[3]
+#     privilege = r[4]
+#     payload = f'bioat={bioat}&factory={factory}&prefix={prefix}&loading_area={loading_area}&privilege={privilege}&is_active=true'
+#     response = requests.request(
+#         "POST", f"{api_host}/order/loading", headers=headers, data=payload)
+#     print(f"create loading Area {loading_area} is: {response.status_code}")
+#     # time.sleep(0.1)
 
 # read loading area
 for a in range(97, 123):
