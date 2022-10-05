dd088f40b991d64a04ede1bdccb4374d25ea5222
Parzival Yuenyang <krumii.it@gmail.com>
2022-10-05 14:58:42 +0700

    update location

--- master.py
+++ master.py
@@ -91,7 +91,7 @@ headers = {
 for a in range(97, 123):
     location = f"S-{str(chr(a)).upper()}"
     for i in range(1, 5):
-        for e in range(1, 20):
+        for e in range(1, 21):
             for j in range(1, 6):
                 title = f"{location}{i:02d}-{e:02d}-{j:02d}"
                 payload = f'title={title}&description={title}&is_active=true'
